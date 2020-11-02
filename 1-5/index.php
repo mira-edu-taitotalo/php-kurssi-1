0a40e503baec25bd4e725de7177500324d83b52e
mira-edu-taitotalo <73821892+mira-edu-taitotalo@users.noreply.github.com>
2020-11-02 16:13:54 +0200

    Update index.php
    First commit – ei voi olla helpoin tapa :D

--- 1-5/index.php
+++ 1-5/index.php
@@ -1,1 +1,4 @@
 <?php
+$taulukko = ['A', 'B', 'C' , 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K'];
+
+echo $taulukko[0] . ' ' . $taulukko[1] . ' ' . $taulukko[2] . ' ' . $taulukko[3] . ' ' . $taulukko[4] . ' ' . $taulukko[5] . ' ' . $taulukko[6] . ' ' . $taulukko[7] . ' ' . $taulukko[8] . ' ' . $taulukko[9] . ' ' . $taulukko[10] . ' ' . $taulukko[11] . '<br>' .  $taulukko[11] . ' ' . $taulukko[10] . ' ' . $taulukko[9] . ' ' . $taulukko[8] . ' ' . $taulukko[7] . ' ' . $taulukko[6] . ' ' . $taulukko[5] . ' ' . $taulukko[4] . ' ' . $taulukko[3] . ' ' . $taulukko[2] . ' ' . $taulukko[1] . ' ' . $taulukko[0];
