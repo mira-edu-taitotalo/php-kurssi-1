836b7b2c6b607f41ef03d6e8bacd522851730a6a
mira-edu-taitotalo <73821892+mira-edu-taitotalo@users.noreply.github.com>
2020-11-03 08:23:54 +0200

    Update index.php

--- 1-7/index.php
+++ 1-7/index.php
@@ -1,1 +1,10 @@
 <?php
+$taulukko = [
+    ['A', 'B', 'C'],
+    ['X', 'Y', 'Z'],
+    ['1', '2', '3']
+];
+
+echo '1. ' . $taulukko[0][0] . ' ' . $taulukko[0][1] . ' ' . $taulukko[0][2] . '<br>';
+echo '2. ' . $taulukko[1][0] . ' ' . $taulukko[1][1] . ' ' . $taulukko[1][2] . '<br>';
+echo '3. ' . $taulukko[2][0] . ' ' . $taulukko[2][1] . ' ' . $taulukko[2][2] . '<br>';
