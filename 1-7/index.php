89b820cbfb9ab174ec02fa1fe9def1c8ad70fce5
mira-edu-taitotalo <73821892+mira-edu-taitotalo@users.noreply.github.com>
2020-11-03 12:36:48 +0200

    Update index.php
    Uusi versio

--- 1-7/index.php
+++ 1-7/index.php
@@ -2,9 +2,17 @@
 $taulukko = [
     ['A', 'B', 'C'],
     ['X', 'Y', 'Z'],
-    ['1', '2', '3']
+    [1, 2, 3]
 ];
 
-echo '1. ' . $taulukko[0][0] . ' ' . $taulukko[0][1] . ' ' . $taulukko[0][2] . '<br>';
+for($i = 0; $i < count($taulukko); $i = $i + 1) {
+    for($j = 0; $j < count($taulukko[$i]); $j = $j +1 ) {
+    echo ' ' . $taulukko[$i][$j];
+    }
+    echo '<br>';
+}
+
+/*echo '1. ' . $taulukko[0][0] . ' ' . $taulukko[0][1] . ' ' . $taulukko[0][2] . '<br>';
 echo '2. ' . $taulukko[1][0] . ' ' . $taulukko[1][1] . ' ' . $taulukko[1][2] . '<br>';
 echo '3. ' . $taulukko[2][0] . ' ' . $taulukko[2][1] . ' ' . $taulukko[2][2] . '<br>';
+*/
