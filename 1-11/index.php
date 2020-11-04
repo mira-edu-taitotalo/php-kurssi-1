5e9bd68dac898202261129fea99f50c36173631a
mira-edu-taitotalo <73821892+mira-edu-taitotalo@users.noreply.github.com>
2020-11-04 18:26:33 +0200

    Update index.php
    First commit

--- 1-11/index.php
+++ 1-11/index.php
@@ -1,1 +1,12 @@
 <?php
+$number = rand (0,5000);
+
+if ($number > 4000) {
+    echo 'A';
+} elseif ($number == 0 || $number == 1000) {
+    echo 'B';
+} elseif ((($number % 2) == 0) && $number < 3000) {
+    echo 'C';
+} else {
+    echo 'D';
+}
